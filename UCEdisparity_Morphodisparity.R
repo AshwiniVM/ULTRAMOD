cb2317fabb1a3b219d13be44a7b7038efef67422
Ashwini V Mohan <ashwinivm30@gmail.com>
2022-08-17 18:39:09 +0100

    Update UCEdisparity_Morphodisparity.R

--- UCEdisparity_Morphodisparity.R
+++ UCEdisparity_Morphodisparity.R
@@ -1,31 +1,18 @@
-#####################Preliminarycomparative pipeline between morphological disparity and a disparity measure created using homologous UCE landmakrs on chromosomes####################################################################
-#######################################################################################################################
-
+#####################Preliminarycomparative pipeline between morphological disparity and a disparity measure created using homologous UCE landmarks on chromosomes####################################################################
 library(ape)
-library(ggplot2)
-library(RColorBrewer)
-setwd("C:/work_mac/PostDoc_MSCA/Github_ULTRAMOD/MMDS_PCA/")
 
+###generating pairwise distance between taxa using UCE landmarks corrected for chromosome size (UCE disparity measure)
 UCEs <- read.csv("sample_presenceonlyUCEs_positionslisted_avgd_sizecorrected.csv", header = T, row.names = 1)
-distance <- as.matrix(dist(UCEs))## Bootstrapping and rarefying
+distance <- as.matrix(dist(UCEs))
 write.csv(distance, file="UCE_disparity_matrix.csv")
 
+###generating pairwise distance between taxa  using UCE landmarks not corrected for chromosome size (UCE disparity measure)
 UCEs_nonsizecorr <- read.csv("sample_presenceonlyUCEs_positionslisted_avgd.csv", header = T, row.names = 1)
-distance <- as.matrix(dist(UCEs_nonsizecorr))## Bootstrapping and rarefying
+distance <- as.matrix(dist(UCEs_nonsizecorr))
 write.csv(distance, file="UCE_disparity_matrix_nonsizecorrected.csv")
 
-###regression
-UCEdisp <- as.data.frame(read.csv("UCE_disp.csv", row.name=1, header=T))
-Phdisp <- as.data.frame(read.csv("disp_phenome.csv", row.name=1, header=T))
+###data type conversions
+UCEdisp <- as.data.frame(read.csv("UCE_disparity_matrix.csv", row.name=1, header=T))
+Geomdisp <- as.data.frame(read.csv("disp_phenome.csv", row.name=1, header=T))
 
-X <- as.matrix(UCEdisp)
-Y <- as.matrix(Phdisp)
-Y
-
-#####A Mantel test to test correlation, this is commonly used to detect spatial structures#######
-man1 <- mantel.test(X,Y)
-man1
-
-
-#####################################################################################################
-#Further steps would include statistical tests for normality, and a suite of statistically modelling tests with other ecological correlates###################
+###Further steps would include statistical tests for normality, and a suite of statistical models to test correlations
